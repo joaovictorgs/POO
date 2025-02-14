4a1184580c7ac408e29ae17f55f938883e961baf
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-02-13 21:41:34 -0300

    first commit

---
+++ aula-1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+        int x=10;
+        int y=20;
+        int z=x+y;
+        System.out.println("O valor de z é: " + z);
+    }
+}
